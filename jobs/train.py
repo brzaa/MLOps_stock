f73f812a239c33f0b79aa54c2bbc4fbc1bbd6031
Bramastya Huseini <bramastyaz@GMAIL.COM>
2024-09-24 22:40:46 +0700

    Update train.py

--- jobs/train.py
+++ jobs/train.py
@@ -1,140 +1,58 @@
-import os
-import argparse
-from sklearn.metrics import mean_absolute_percentage_error
 import pytorch_lightning as pl
-import numpy as np 
-from torch.utils.data import DataLoader, TensorDataset
 import torch
+from torch import nn
 import pandas as pd
-import pickle
-from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import MinMaxScaler
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-import mlflow.pytorch
+import mlflow
 
-class dataset(pl.LightningDataModule):
+class StockPriceModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=1, hidden_size=64, num_layers=2, batch_first=True)
+        self.linear = nn.Linear(64, 1)
 
-    def __init__(self, data=None, scaler=None):
-        super(dataset,self).__init__()
-        self.lookback_size = 5
-        self.batch_size = 32
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        return self.linear(x[:, -1])
 
-        if data is not None:
-            self.data=data
-            self.series = pd.read_csv(self.data, index_col='Date')
-            # Train:valid:test = 80:10:10
-            self.train_df, self.valid_df = train_test_split(self.series, test_size=0.2)
-            self.valid_df, self.test_df = train_test_split(self.valid_df, test_size=0.5)
-
-        if scaler is None and data is not None:
-            self.scaler = MinMaxScaler().fit(self.train_df)
-        else:
-            self.scaler=scaler
-
-    def train_tensors(self,df):
-
-        X, y = [], []
-
-        for i in np.arange(self.lookback_size, len(df)-1):
-            X.append(df[i-self.lookback_size:i])
-            y.append(df[i+1])
-
-        X = np.array(X).reshape(-1,self.lookback_size,1)
-        y = np.array(y).reshape(-1,1)
-        return TensorDataset(torch.from_numpy(X), torch.from_numpy(y))
-    
-    def setup(self, stage=None):
-        
-        self.train_df = self.scaler.transform(self.train_df) 
-        self.valid_df = self.scaler.transform(self.valid_df) 
-        self.test_df = self.scaler.transform(self.test_df) 
-
-        self.train_data = self.train_tensors(self.train_df)
-        self.valid_data = self.train_tensors(self.valid_df)
-        self.test_data = self.train_tensors(self.test_df)
-
-    def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=self.batch_size)
-
-    def val_dataloader(self):
-       return DataLoader(self.valid_data, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-       return DataLoader(self.test_data, batch_size=self.batch_size)
-class model(pl.LightningModule):
-
-    def __init__(self,lookback_size=5):
-
-        super(model,self).__init__()
-
-        self.lookback_size = lookback_size
-        self.lstm=torch.nn.LSTM(batch_first=True, input_size=1, hidden_size=self.lookback_size)
-        self.out=torch.nn.Linear(5,1)
-        self.loss=torch.nn.functional.mse_loss
-
-    def forward(self, x, hidden=None):
-        x, hidden = self.lstm(x)
-        x = x[:,-1]
-        x = self.out(x)
-        return x, hidden
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(params=self.parameters(), lr=1e-3)
-
-    def configure_callbacks(self):
-        early_stop = EarlyStopping(monitor="val_loss", mode="min", patience=5)
-        checkpoint = ModelCheckpoint(monitor="val_loss")
-        return [early_stop, checkpoint]
-    
-    def training_step(self, train_batch, batch_idx):
-        x, y = train_batch 
-        logits,_ = self.forward(x.type(torch.float32)) 
-        loss = self.loss(logits.float(), y.float()) 
-        self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = nn.MSELoss()(y_hat, y)
+        self.log('train_loss', loss)
         return loss
 
-    def validation_step(self, valid_batch, batch_idx): 
-        x, y = valid_batch 
-        logits,_ = self.forward(x.type(torch.float32)) 
-        loss = self.loss(logits.float(), y.float())
-        self.log("val_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
 
-    def test_step(self, test_batch, batch_idx): 
-        x, y = test_batch 
-        logits,_ = self.forward(x.type(torch.float32)) 
-        loss = self.loss(logits.float(), y.float())
-        self.log("test_loss", loss)
-
-def train(args):
-
-    trainer=pl.Trainer(max_epochs=5, enable_progress_bar=True)
-    mlflow.pytorch.autolog()
-    datamod=dataset(args.data)
-    mod=model()
-    with mlflow.start_run() as run:
-        trainer.fit(model=mod, datamodule=datamod)
-
-    trainer.test(model=mod, datamodule=datamod)
-
-    return mod, datamod.scaler
-
-def main():
-    """Main function of the script."""
-
-    # input and output arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, help="Path to input data")
-
-    args = parser.parse_args()
+def prepare_data(data_path):
+    df = pd.read_csv(data_path)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values('Date')
     
-    # Load Scaler object later and send it for scaling data
-
-    trainedModel, scalerObj = train(args)
-
-    pickle.dump(scalerObj, open('./outputs/scaler.pkl','wb'))
-    model_file = f"./outputs/model.pth"
-    torch.save(trainedModel.state_dict(), model_file)
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(df['Close'].values.reshape(-1, 1))
+    
+    sequences = []
+    targets = []
+    for i in range(len(scaled_data) - 30):
+        sequences.append(scaled_data[i:i+30])
+        targets.append(scaled_data[i+30])
+    
+    return torch.FloatTensor(sequences), torch.FloatTensor(targets)
 
 if __name__ == "__main__":
-    main()
+    mlflow.pytorch.autolog()
+
+    data_path = "path/to/MASB.JK.csv"  # Update this path
+    X, y = prepare_data(data_path)
+    
+    dataset = TensorDataset(X, y)
+    train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+    
+    model = StockPriceModel()
+    trainer = pl.Trainer(max_epochs=50)
+    trainer.fit(model, train_loader)
+
+    mlflow.pytorch.log_model(model, "model")
